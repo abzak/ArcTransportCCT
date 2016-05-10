1f242353f5704f911dc1572a2d15b7755f4d01a7
Chloé Trachsel <chloe.trachsel@he-arc.ch>
2016-05-10 11:14:00 +0200

    Vehicule done
    Classe vehicule

--- src/arctransport/metier/Vehicule.java
+++ src/arctransport/metier/Vehicule.java
@@ -5,6 +5,10 @@
  */
 package arctransport.metier;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 /**
  *
  * @author chloe.trachsel
@@ -13,5 +17,84 @@ package arctransport.metier;
 //Chloé Trachsel
 
 public class Vehicule {
+    /*Taxe de base
+        0 - 5 km --> 10.-
+        6 - 10km --> 8.-
+         > 10 km --> 5.-
+    */
+    
+    private int matricule;
+    private String designation;
+    private int prixMinute;
+    private List<Course> listeCourses;
+
+    public Vehicule() {
+        listeCourses = new ArrayList<>();
+    }
+
+    public Vehicule(int matricule, String designation, int prixMinute, Course listeCourses) {
+        this();
+        this.matricule = matricule;
+        this.designation = designation;
+        this.prixMinute = prixMinute;
+    }
+
+    public int getMatricule() {
+        return matricule;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public int getPrixMinute() {
+        return prixMinute;
+    }
+
+    public List<Course> getListeCourses() {
+        return listeCourses;
+    }
+
+    public void setMatricule(int matricule) {
+        this.matricule = matricule;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public void setPrixMinute(int prixMinute) {
+        this.prixMinute = prixMinute;
+    }
+
+    public void setListeCourses(List<Course> listeCourses) {
+        this.listeCourses = listeCourses;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 89 * hash + Objects.hashCode(this.matricule);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Vehicule other = (Vehicule) obj;
+        if (!Objects.equals(this.matricule, other.matricule)) {
+            return false;
+        }
+        if (!Objects.equals(this.designation, other.designation)) {
+            return false;
+        }
+        return true;
+    }
+    
     
 }
