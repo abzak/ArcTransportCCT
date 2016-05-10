e05854d6f29bcb8ae28436759270bb78f9408a1f
abzak <zakaria.chentouf@he-arc.ch>
2016-05-10 10:57:17 +0200

    Chauffeur done
    I love nice and weet food

--- src/arctransport/metier/Chauffeur.java
+++ src/arctransport/metier/Chauffeur.java
@@ -5,13 +5,99 @@
  */
 package arctransport.metier;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 /**
  *
- * @author chloe.trachsel
+ * @author Zakaria Chentouf
+ * @mail zakaria.chentouf@he-arc.ch
  */
 
 //Zakaria Chentouf
 
 public class Chauffeur {
+    private String matricule;
+    private String nom;
+    private String prenom;
+    private List<Course> courses;
+
+    public Chauffeur() {
+        courses = new ArrayList<>();
+    }
+
+    public Chauffeur(String matricule, String nom, String prenom) {
+        this();
+        this.matricule = matricule;
+        this.nom = nom;
+        this.prenom = prenom;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 89 * hash + Objects.hashCode(this.matricule);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Chauffeur other = (Chauffeur) obj;
+        if (!Objects.equals(this.matricule, other.matricule)) {
+            return false;
+        }
+        if (!Objects.equals(this.nom, other.nom)) {
+            return false;
+        }
+        if (!Objects.equals(this.prenom, other.prenom)) {
+            return false;
+        }
+        return true;
+    }
+    
+    public float getChiffreDAffaire(){
+     return 0;
+    }
+    
+    public String getMatricule() {
+        return matricule;
+    }
+
+    public void setMatricule(String matricule) {
+        this.matricule = matricule;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
+    }
+    
+    
     
 }
