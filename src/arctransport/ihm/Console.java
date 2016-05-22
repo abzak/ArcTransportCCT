c3b4cf91ded87967faea6ffb460eb4019a1f0cac
sagarion <thibault.daucourt@he-arc.ch>
2016-05-22 17:00:25 +0200

    update console
    ajout des différents menus à al console

--- src/arctransport/ihm/Console.java
+++ src/arctransport/ihm/Console.java
@@ -13,10 +13,43 @@ public class Console {
     public void AfficheMenuFlotte(){
         System.out.println("");
         System.out.println("-- Menu Principale de la Flotte--");
-        System.out.println("'1' -- Gestion des Chauffeurs");
-        System.out.println("'2' -- Gestion des Véhicules");
-        System.out.println("'3' -- Gestion des Courses");
-        System.out.println("'4' -- Quitter");
+        System.out.println("[] -- Gestion des Chauffeurs");
+        System.out.println("[] -- Gestion des Véhicules");
+        System.out.println("[] -- Gestion des Courses");
+        System.out.println("[] -- Calculer le chiffer d'affaire total");
+        System.out.println("[] -- Quitter");
+        System.out.println("");
+    }
+    
+    public void AfficheMenuChauffeur(){
+        System.out.println("");
+        System.out.println("-- Menu Gestion des chauffeurs --");
+        System.out.println("[] -- Afficher tous les Chauffeurs");
+        System.out.println("[] -- Calculer le chiffer d'affaire d'un chauffeur");
+        System.out.println("[] -- Afficher un chauffeur (matricule)");
+        System.out.println("[] -- Ajouter un chauffeur");
+        System.out.println("[] -- Supprimer un chauffeur");
+        System.out.println("[] -- retour");
+        System.out.println("");
+    }
+    
+    public void AfficheMenuCourse(){
+        System.out.println("");
+        System.out.println("-- Menu Gestion des Courses --");
+        System.out.println("[] -- Afficher toutes les Courses");
+        System.out.println("[] -- Ajouter un Course");
+        System.out.println("[] -- Supprimer un Course");
+        System.out.println("[] -- retour");
+        System.out.println("");
+    }
+    
+    public void AfficheMenuVehicule(){
+        System.out.println("");
+        System.out.println("-- Menu Gestion des Véhicules --");
+        System.out.println("[] -- Afficher toutes les Véhicules");
+        System.out.println("[] -- Ajouter un Véhicule");
+        System.out.println("[] -- Supprimer un Véhicule");
+        System.out.println("[] -- retour");
         System.out.println("");
     }
 }
